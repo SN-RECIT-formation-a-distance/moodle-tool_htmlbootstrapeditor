741e09718900304ea8920df63bbf8c6656faf4ad
Gustavo Bazzo <gcbazzo@gmail.com>
2023-06-19 15:37:37 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023032801;
+$plugin->version   = 2023032802;
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'tool_htmlbootstrapeditor';
-$plugin->release = 'v1.0.3-beta';
+$plugin->release = 'v1.0.4-stable';
 $plugin->supported = [39, 410];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
-$plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
+$plugin->maturity = MATURITY_STABLE; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
