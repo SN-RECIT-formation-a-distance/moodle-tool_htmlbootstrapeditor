9267cb47509ae24f821e2b4f0917de47200ab7ba
mitchell-accipio <mitchell@accipio.com>
2024-04-23 17:47:17 +0100

    restructure repo to moodle standard

--- sync.ps1
+++
@@ -1,9 +0,0 @@
-$from = "moodle-tool_htmlbootstrapeditor/src/*"
-$to = "shared/recitfad/admin/tool/htmlbootstrapeditor"
-
-try {
-    . ("..\sync\watcher.ps1")
-}
-catch {
-    Write-Host "Error while loading sync.ps1 script." 
-}
