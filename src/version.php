db7476d663ab0c3ed6cfaf12005c867ea2ba6604
Gustavo Bazzo <gcbazzo@gmail.com>
2025-01-09 16:03:32 -0500

    Fix flipcard on Firefox

--- src/version.php
+++ src/version.php
@@ -19,6 +19,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2024121601;
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'tool_htmlbootstrapeditor'; 
-$plugin->release = 'v1.1.0-stable'; 
+$plugin->release = 'v1.1.1-stable'; 
 $plugin->supported = [39, 405];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
 $plugin->maturity = MATURITY_STABLE; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
