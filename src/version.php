258608c96e5f39ac82cef91df92e714b4b333777
Gustavo Bazzo <gcbazzo@gmail.com>
2025-05-13 14:09:13 -0400

    Lib and version upgrade

--- src/version.php
+++ src/version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025013000;
+$plugin->version   = 2025051300;
 $plugin->requires  = 2024071200.00; // Moodle 4.5.0
 $plugin->component = 'tool_htmlbootstrapeditor'; 
-$plugin->release = 'v2.0.0-stable'; 
+$plugin->release = 'v2.0.1-stable'; 
 $plugin->supported = [405, 405];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
 $plugin->maturity = MATURITY_STABLE; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
