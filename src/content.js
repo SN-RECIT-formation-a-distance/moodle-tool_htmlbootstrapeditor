76c90481fb37ea336e04535fef8a22ad1c853392
Gustavo Bazzo <gcbazzo@gmail.com>
2025-08-22 11:37:12 -0400

    Remove jquery

--- src/content.js
+++ src/content.js
@@ -29,9 +29,12 @@ M.recit.htmlbootstrapeditor.Popup = class {
     constructor(content) {        
         let modal = document.createElement('div');
         modal.classList.add('modal', 'fade', 'htmlbootstrapeditor_popup');
+        modal.setAttribute('style', 'overflow-y: hidden;');
+
         let inner2 = document.createElement('div');
         inner2.classList.add('modal-dialog');
         modal.appendChild(inner2);
+
         let inner = document.createElement('div');
         inner.classList.add('modal-content');
         inner2.appendChild(inner);
@@ -39,10 +42,12 @@ M.recit.htmlbootstrapeditor.Popup = class {
         let header = document.createElement('div');
         header.classList.add('modal-header');
         inner.appendChild(header);
+
         let btn = document.createElement('button');
         btn.classList.add('close');
         btn.innerHTML = '<span aria-hidden="true">&times;</span>';
         btn.setAttribute('data-dismiss', 'modal');
+        btn.onclick = this.destroy.bind(this);
         header.appendChild(btn);
         
         let body = document.createElement('div');
@@ -52,19 +57,25 @@ M.recit.htmlbootstrapeditor.Popup = class {
         
         document.body.appendChild(modal);
         this.popup = modal;
-        $(modal).modal({show: true, backdrop: true});
-        let that = this;
-        $(".modal-backdrop").click(() => $(this.popup).modal('hide'));
-        $(modal).on('hidden.bs.modal', function (e) {
-            that.destroy()
-        })
-      }
-      destroy(){
-          this.popup.remove();
-      }
-      update(){
-        $(this.popup).modal('handleUpdate');
-      }
+
+        this.popup.classList.add('show');
+
+        this.backdrop = document.createElement('div');
+        this.backdrop.classList.add('modal-backdrop', 'fade', 'show');
+        this.backdrop.setAttribute('data-backdrop', 'static');
+        document.body.appendChild(this.backdrop);
+    }
+
+    destroy(){
+        this.popup.classList.remove('show');
+        this.backdrop.classList.remove('show');
+        this.popup.remove();
+        this.backdrop.remove();
+    }
+
+    update(){
+    
+    }
 }
 
 document.body.addEventListener('click',function(e){
