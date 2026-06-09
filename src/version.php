a729999062a3723c4e7e85af0b2cc72b2b37bc97
Gustavo Bazzo <gcbazzo@gmail.com>
2026-06-09 13:55:14 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026051200;
+$plugin->version   = 2026051201;
 $plugin->requires  = 2025100603.00; // Moodle 5.1.3
 $plugin->component = 'tool_htmlbootstrapeditor'; 
-$plugin->release = 'v3.0.0-stable'; 
+$plugin->release = 'v3.0.1-stable'; 
 $plugin->supported = [501, 501];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
 $plugin->maturity = MATURITY_STABLE; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
